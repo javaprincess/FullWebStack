c4e18a9be40e2df3728a9ae54ccb5a200b01218f
AndreasM <AndreasM@TCFILMD50507780.ffe.foxeg.com>
2015-01-22 09:20:06 -0800

    Incremented version

--- erm/src/com/fox/it/erm/Version.java
+++ erm/src/com/fox/it/erm/Version.java
@@ -1,7 +1,7 @@
 package com.fox.it.erm;
 
 public class Version {
-	public static final String VERSION="1.2-b22";
+	public static final String VERSION="1.2-b24";
 	public static final String V="v=" + VERSION;
 	public static final String CODE_NAME = "";
 	
